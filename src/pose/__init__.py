864e23c2c7992b70501b357072643b8199aa4a7c
hwanhee <h2park98@gmail.com>
2026-04-15 15:44:04 +0900

    UI 1차 개선

--- src/pose/__init__.py
+++ src/pose/__init__.py
@@ -2,5 +2,6 @@
 from .detector import PoseDetector
 from .landmark_utils import LandmarkProcessor
 from .visualizer import PoseVisualizer
+from .silhouette_renderer import SilhouetteRenderer
 
-__all__ = ["PoseDetector", "LandmarkProcessor", "PoseVisualizer"]
+__all__ = ["PoseDetector", "LandmarkProcessor", "PoseVisualizer", "SilhouetteRenderer"]
